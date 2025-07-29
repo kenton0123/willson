6df14abfe088c8a2d3209c3d0d63ca3a36c92ae1
kenton0123 <abckentonlam@gmail.com>
2025-07-29 14:45:19 +0800

    Update scenario5.py

--- pages/scenario5.py
+++ pages/scenario5.py
@@ -1,11 +1,9 @@
 # disney_scenario3.py
 import streamlit as st
 from config.template import create_museum_scenario_page
-from config.utils import capture_and_modify_status
-
 
 def main():
-    capture_and_modify_status()
+    
     custom_star_rating = 1.5  # 自定义星级评分值 (0.0-5.0)
     custom_rating_count = 12  # 自定义评分人数 (以K为单位，例如150.5表示150,500人)
     custom_level_confidence = 2

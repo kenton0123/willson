07be1b50a3fba1f4177525c805e20a3d3266eea9
kenton0123 <abckentonlam@gmail.com>
2025-07-29 15:51:02 +0800

    Update case5.py

--- pages/case5.py
+++ pages/case5.py
@@ -144,14 +144,12 @@ Instruction: Please copy the following question to receive background informatio
             # Get the state parameter from session state
             state = st.session_state.get('state', '')
             
-            # Create the URL with state parameter
-            next_page_url = f"pages/scenario5?state={state}"
-            
-            # Create the button with the dynamic URL
-            st.markdown(
-                f'<a href="{next_page_url}" class="stPageLink">开始作业 <span role="img">🚀</span></a>',
-                unsafe_allow_html=True
+            st.page_link(
+                f"pages/scenario5.py?state={state}",
+                label="开始作业",
+                icon="🚀"
             )
+
             
             st.markdown('</div>', unsafe_allow_html=True)
 

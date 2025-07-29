81a3004e7172460150a553bd1843797cfe01fda8
kenton0123 <abckentonlam@gmail.com>
2025-07-29 15:15:47 +0800

    Update scenario5.py

--- pages/scenario5.py
+++ pages/scenario5.py
@@ -1,14 +1,19 @@
 # disney_scenario3.py
 import streamlit as st
 from config.template import create_museum_scenario_page
+from config.utils import capture_and_modify_state
 
 def main():
-    
+    capture_and_modify_state(display=False)
+
     custom_star_rating = 1.5  # 自定义星级评分值 (0.0-5.0)
     custom_rating_count = 12  # 自定义评分人数 (以K为单位，例如150.5表示150,500人)
     custom_level_confidence = 2
+    state = st.session_state.get('state', '')
+
     survey_href = "https://hkbu.questionpro.com/t/AVqakZ59Ai"  # 自定义调查链接
     # 使用自定义值调用创建页面函数
+    survey_href = f"{survey_base}?state={state}"
     create_museum_scenario_page(
         scenario_num=5,
         custom_star_rating=custom_star_rating,

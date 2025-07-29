046e4a12f2a0b570edc64cf55264af9f054bec9c
kenton0123 <abckentonlam@gmail.com>
2025-07-29 14:34:55 +0800

    Update case5.py

--- pages/case5.py
+++ pages/case5.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from utils import capture_and_modify_status
 
 # Set page configuration
 st.set_page_config(
@@ -9,6 +10,7 @@ st.set_page_config(
 
 
 def main():
+    capture_and_modify_status()
     # Custom CSS for a professional academic look
     st.markdown("""
         <style>
